e1c58d75a5db633bc9ee99b59b529c33b8188d95
karenokasaki <oks.karen@gmail.com>
2024-07-22 16:57:08 -0300

    Init

---
+++ functions-and-arrays.js
@@ -0,0 +1,77 @@
+// Iteration 1 | Find the Maximum
+function maxOfTwoNumbers(number1, number2) {
+   //write inside the function
+}
+console.log("max of two numbers -->", maxOfTwoNumbers(10, 20));
+
+// Iteration 2 | Find the Longest Word
+function findLongestWord() {
+   //write inside the function
+}
+const words = [
+   "mystery",
+   "brother",
+   "aviator",
+   "crocodile",
+   "pearl",
+   "orchard",
+   "crackpot",
+]; //estamos passando essa array pra dentro da função
+console.log("findLongestWord -->", findLongestWord(words));
+
+// Iteration 3 | Sum Numbers | Faça uma função que some todos os números de um array
+function sumNumbers() {
+   //write inside the function
+}
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10]; //estamos passando essa array pra dentro da função
+console.log("sumNumbers -->", sumNumbers(numbers));
+
+// Iteration 4 | Numbers Average | Faça uma função que calcule a média de um array
+function averageNumbers() {
+   //write inside the function
+}
+const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9]; //estamos passando essa array pra dentro da função
+console.log("averageNumbers -->", averageNumbers(numbers2));
+
+// Iteration 5 | Find Elements | Faça uma função que verifique se um elemento existe em um array
+function doesWordExist() {
+   //write inside the function
+}
+const words2 = [
+   "machine",
+   "subset",
+   "trouble",
+   "starting",
+   "matter",
+   "eating",
+   "truth",
+   "disobedience",
+]; //estamos passando essa array pra dentro da função
+console.log("doesWordExist -->", doesWordExist(words2));
+
+// Iteration 6 | Reverse String | Faça uma função que inverta uma string
+function reverseString(string) {
+   //write inside the function
+}
+console.log("reverseString -->", reverseString("hello"));
+
+// Iteration 7 | Converting Dollars into Reais | Faça uma função que converta dólares em reais
+function convertTo(dollars) {
+   //write inside the function
+}
+console.log("convertTo -->", convertTo(100));
+
+// Iteration 8 | Nuymber sequence | Faça uma função que cria um array com números de 0 a n
+function createArray(n) {
+   //write inside the function
+}
+console.log("createArray -->", createArray(10));
+// expected output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+// BONUS 1 | Polindrome | Faça uma função que verifique se uma palavra é palíndrome = se a palavra é igual de trás pra frente
+function isPalindrome(word) {
+   //write inside the function
+}
+console.log("isPalindrome -->", isPalindrome("race car"));
